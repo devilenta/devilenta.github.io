fa4846cefa42ae9c77215a5e5df2630e1b08bd9c
devilenta <mohsenhosseini116@gmail.com>
2025-04-13 13:05:15 +0330

    fix bug and test new page

---
+++ script/script_articles.js
@@ -0,0 +1,9 @@
+let window_art = document.querySelector(".window_art")
+
+
+
+
+window_art.addEventListener("click",()=>{
+
+    window.location.href = ("https://www.brilsoft.ir/download/brilclubn.exe")
+})
