c468e730276ccac8c6aab72d2aefd5c5fd9954df
devilenta <mohsenhosseini116@gmail.com>
2025-04-18 19:07:15 +0330

    fix more bug

--- script/script_articles.js
+++ script/script_articles.js
@@ -1,4 +1,6 @@
 let window_art = document.querySelector(".window_art")
+let android_art = document.querySelector(".android_art")
+let iphone_art = document.querySelector(".iphone_art")
 
 
 
@@ -6,4 +8,8 @@ let window_art = document.querySelector(".window_art")
 window_art.addEventListener("click",()=>{
 
     window.location.href = ("https://www.brilsoft.ir/download/brilclubn.exe")
+})
+android_art.addEventListener("click",()=>{
+
+    window.location.href = ("https://www.brilsoft.ir/download/brilclub.apk")
 })
